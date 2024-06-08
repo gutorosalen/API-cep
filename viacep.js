0882264c6e046ce858d32b18ce96dc6ac7bac031
unknown <gutorosalen.gustavorosalen@hotmail.com>
2024-06-08 04:38:24 -0300

    v.1

---
+++ viacep.js
@@ -0,0 +1,13 @@
+async function getcep() {
+  const cep = document.getElementById("cep").value;
+  try {
+    const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    const data = await response.json();
+    console.log(data);
+    document.getElementById("rua").value = data.logradouro;
+    document.getElementById("bairro").value = data.bairro;
+    document.getElementById("cidade").value = data.localidade;
+  } catch (error) {
+    alert("Cep Inválido");
+  }
+}
